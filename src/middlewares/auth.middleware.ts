1d3fe79a9a474bfb6c4dd8bd092dd9bdeb08e5f5
tkdgh7063 <hoo412298@gmail.com>
2026-03-23 16:20:18 +0900

    [feat] 추가: 사용자 인증 미들웨어

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Request, Response, NextFunction } from 'express';
 
 export const authMiddleware = (
@@ -12,3 +13,56 @@ export const authMiddleware = (
 
   next();
 };
+=======
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { StatusCodes } from 'http-status-codes';
+
+import prisma from '../config/prisma';
+
+export const authUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    let { token } = req.cookies;
+
+    if (!token) {
+      // 쿠키 부여
+      const randomUsername = 'asdf';
+
+      const newUser = await prisma.user.create({
+        data: {
+          username: randomUsername,
+          updated_at: new Date(),
+        },
+      });
+
+      token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET as string, {
+        expiresIn: '7d',
+      });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+
+      req.user = { id: newUser.id };
+      return next();
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+      id: number;
+    };
+
+    req.user = { id: decoded.id };
+
+    return next();
+  } catch (err) {
+    console.error('Authenticate Error: ', err);
+    res.clearCookie('token');
+    return res.status(StatusCodes.UNAUTHORIZED).json();
+  }
+};
+>>>>>>> 4af8ff9 ([feat] 추가: 사용자 인증 미들웨어)
