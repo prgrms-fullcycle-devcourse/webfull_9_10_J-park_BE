1571c7654af732c633fbe40b66b0b45771c626e7
tkdgh7063 <hoo412298@gmail.com>
2026-04-11 21:27:08 +0900

    [feat] 추가: s3 스토리지 설정

---
+++ src/middlewares/upload.middleware.ts
@@ -0,0 +1,20 @@
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
+
+import { s3 } from '../config/s3Storage';
+
+export const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.S3_BUCKET_NAME!,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (_, file, cb) => {
+      const ext = path.extname(file.originalname); // 확장자
+      const fileName = `profiles/${uuidv4()}${ext}`; // 파일명 설정
+      cb(null, fileName);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB 미만
+});
