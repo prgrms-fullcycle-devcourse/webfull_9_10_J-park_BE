a2fe9a17b1d42ff22bbf63cf6ba501d09bd59fad
jhleejh98-boop <jhleejh98@gmail.com>
2026-04-13 16:48:51 +0900

    [test] 수정: uuid mock 전역 적용 및 테스트 안정화

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
   verbose: true,
   silent: true, // 예기치 못한 오류까지 보고 싶을 경우 주석 처리하세요
   transformIgnorePatterns: ['/node_modules/(?!(?:@faker-js/faker)/)'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'], // 모든 테스트 실행 전에 공통 setup 파일 실행
 };
 
 export default config;
