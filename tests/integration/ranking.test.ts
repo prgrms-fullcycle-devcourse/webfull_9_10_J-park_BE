9365cb790c123f6d70b30b67a041226234739761
jhleejh98-boop <jhleejh98@gmail.com>
2026-04-19 18:53:03 +0900

    [fix] 수정 : 주석 추가 및 삭제

--- tests/integration/ranking.test.ts
+++ tests/integration/ranking.test.ts
@@ -14,6 +14,7 @@ describe('Ranking API', () => {
   let myUserId: number;
 
   beforeEach(async () => {
+    // ranking 정렬을 위해 기존 데이터보다 높은 값 사용
     await delCacheByPattern('lampfire:ranking:*');
 
     const user1 = await prisma.user.create({
@@ -101,7 +102,7 @@ describe('Ranking API', () => {
   afterAll(async () => {
     await prisma.$disconnect();
   });
-
+  // 전체 랭킹 조회
   describe('GET /rankings', () => {
     describe('200 - OK', () => {
       it('실제 DB 데이터를 기준으로 랭킹 목록을 조회한다', async () => {
@@ -172,7 +173,7 @@ describe('Ranking API', () => {
         }
       });
     });
-
+    // 401 - 잘못된 토큰일 경우, 인증 에러를 반환한다
     describe('401 - UNAUTHORIZED', () => {
       it('인증되지 않은 요청일 경우 반환한다', async () => {
         const invalidToken = jwt.sign({ id: 999999 }, process.env.JWT_SECRET!, {
@@ -195,7 +196,7 @@ describe('Ranking API', () => {
         );
       });
     });
-
+    // 500 - 서버 오류 시 명세된 에러 형식으로 반환한다
     describe('500 - INTERNAL_SERVER_ERROR', () => {
       it('서버 내부 예외가 발생할 경우 반환한다', async () => {
         const serviceSpy = jest
@@ -205,7 +206,8 @@ describe('Ranking API', () => {
         const res = await request(app)
           .get('/rankings')
           .set('Cookie', [`token=${authToken}`]);
-
+        // console.log('Status:', res.status);
+        // console.log('Body:', JSON.stringify(res.body, null, 2));
         expect(res.status).toBe(500);
         expect(res.body).toEqual(
           expect.objectContaining({
